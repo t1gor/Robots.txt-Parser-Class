0b7eb2384abba20d69963ef223b31552c56afeab
Eugene Yurkevich <yurkevich@vicman.net>
2014-12-05 11:50:39 +0300

    add failed test with whitespaces

---
+++ test/WithWhiteSpaces.php
@@ -0,0 +1,46 @@
+<?php
+	/**
+	 * @backupGlobals disabled
+	 */
+	class WithWhiteSpacesTest extends \PHPUnit_Framework_TestCase
+	{
+		/**
+		 * Load library
+		 */
+		public static function setUpBeforeClass()
+		{
+			require_once(realpath(__DIR__.'/../robotstxtparser.php'));
+		}
+
+		/**
+		 * @dataProvider generateDataForTest
+		 * @covers RobotsTxtParser::isDisallowed
+		 * @covers RobotsTxtParser::checkRule
+		 * @param string $robotsTxtContent
+		 */
+		public function testContainingAtChar($robotsTxtContent)
+		{
+			// init parser
+			$parser = new RobotsTxtParser($robotsTxtContent);
+			$this->assertInstanceOf('RobotsTxtParser', $parser);
+
+			$this->assertTrue(!empty($this->rules['*']), 'rules for * is empty');
+			$this->assertFalse($parser->isDisallowed("/admin"), 'failed disallowed');
+			$this->assertTrue($parser->isAllowed("/admin/front"), 'failed allow');
+		}
+
+		/**
+		 * Generate test case data
+		 * @return array
+		 */
+		public function generateDataForTest()
+		{
+			return array(
+				array("
+					User-agent: *
+					Disallow : /admin
+					Allow    :   /admin/front
+				")
+			);
+		}
+	}
